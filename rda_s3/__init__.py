6342618cc5f8e08cadb423810be2ed0232643df6
Thomas Cram <tcram@ucar.edu>
2020-03-10 15:49:49 -0600

    Added logging

--- rda_s3/__init__.py
+++ rda_s3/__init__.py
@@ -1,1 +1,6 @@
+import logging
 
+# configure logging for a library, per python best practices:
+# https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+# NB: this won't work on py2.6 because `logging.NullHandler` wasn't added yet
+logging.getLogger("rda_s3").addHandler(logging.NullHandler())
