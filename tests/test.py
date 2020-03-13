3828eca0ecb62fa4306b0a17c9e306fc945d5d67
Riley Conroy <rpconroy@ucar.edu>
2020-03-13 10:26:21 -0600

    update

--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,10 @@
 """
 Test rda_s3.py
 """
-import inspect
 import sys
 import os
+import inspect
+import pdb
 sys.path.append(os.path.dirname(os.path.abspath(__file__))+'/..')
 from rda_s3 import rda_s3
 
@@ -14,16 +15,24 @@ def passed():
     print('Passed ', calframe[1][3])
 
 def test_argparse():
+    parser = rda_s3._get_parser()
+    args = ['lb', '-bo']
+    args = parser.parse_args(args)
     passed()
 
 def test_list_objects():
+    ret = rda_s3.main('lo', )
     passed()
 
 def test_list_buckets():
+    ret = rda_s3.main('lb', )
     passed()
 
-test_argparse()
-test_list_objects()
-test_list_buckets()
+# Run functions that start with 'test'
+funcs = list(filter(lambda x: x[:4] == 'test', dir()))
+self = sys.modules[__name__]
+for func_str in funcs:
+    func = getattr(self, func_str)
+    func()
 
 
